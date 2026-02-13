478b06e5072cf4369058672eeda9a172e803ecc8
drguandinango <drguandinango@pucesi.edu.ec>
2026-02-13 09:50:49 -0500

    interface

--- pepasisismo/projectNest/src/usuarios/usuarios.controller.ts
+++ pepasisismo/projectNest/src/usuarios/usuarios.controller.ts
@@ -12,7 +12,7 @@ export class UsuariosController {
 
   @Get()
   findAll() {
-    return this.usuariosService.findAll();
+    return this.usuariosService.getDatosServ();
   }
 
 
--- pepasisismo/projectNest/src/usuarios/usuarios.service.ts
+++ pepasisismo/projectNest/src/usuarios/usuarios.service.ts
@@ -2,16 +2,34 @@ import { Injectable } from '@nestjs/common';
 // import { CreateUsuarioDto } from './dto/create-usuario.dto';
 // import { UpdateUsuarioDto } from './dto/update-usuario.dto';
 
+
+export interface User {
+  name: string;
+  age: number;
+}
 @Injectable()
 export class UsuariosService {
   create() {
     return 'This action adds a new usuario';
   }
 
-  findAll() {
-    return `This action returns all usuarios`;
-  }
+ 
 
+  getDatosServ(): User { 
+
+  
+
+  
+
+        return{ 
+
+            name: 'Jhon Doe', 
+
+            age: 40 
+
+        } 
+
+    } 
   // findOne(id: number) {
   //   return `This action returns a #${id} usuario`;
   // }
