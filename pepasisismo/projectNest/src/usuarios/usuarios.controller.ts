60d9e307252b82dd0c35db72d1ca49c8555a9ed7
drguandinango <drguandinango@pucesi.edu.ec>
2026-02-13 09:31:54 -0500

    [200~Post

---
+++ pepasisismo/projectNest/src/usuarios/usuarios.controller.ts
@@ -0,0 +1,45 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsuariosService } from './usuarios.service';
+
+
+@Controller('/usuarios')
+export class UsuariosController {
+  constructor(private readonly usuariosService: UsuariosService) {}
+ @Post()
+  create() {
+    return this.usuariosService.create();
+  }
+
+  @Get()
+  findAll() {
+    return this.usuariosService.findAll();
+  }
+
+
+
+
+  // @Post()
+  // create(@Body() createUsuarioDto: CreateUsuarioDto) {
+  //   return this.usuariosService.create(createUsuarioDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   return this.usuariosService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usuariosService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
+  //   return this.usuariosService.update(+id, updateUsuarioDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usuariosService.remove(+id);
+  // }
+}
