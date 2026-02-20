804173354f810285aea6b652cf0cc446b539c731
drguandinango <drguandinango@pucesi.edu.ec>
2026-02-20 11:50:36 -0500

    HTTP STATUS+PIPES pilitas mijines Proyecto incial\project

--- Proyecto incial/project/src/app.module.ts
+++ Proyecto incial/project/src/app.module.ts
@@ -4,9 +4,10 @@ import { TasksModule } from './tasks/tasks.module';
 // import { AppController } from './app.controller';
 // import { AppService } from './app.service';
 import { UsersaModule } from './usersa/usersa.module';
+import { HelloModule } from './hello/hello.module';
 
 @Module({
-  imports: [TasksModule, UsersaModule],
+  imports: [TasksModule, UsersaModule, HelloModule],
   // controllers: [AppController],
   // providers: [AppService],
 })
---
+++ Proyecto incial/project/src/hello/hello.controller.ts
@@ -0,0 +1,66 @@
+import {
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  ParseBoolPipe,
+  ParseIntPipe,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
+import { ValidateuserPipe } from './pipes/validateuser/validateuser.pipe';
+@Controller()
+export class HelloController {
+  @Get('/')
+  index(@Req() request: Request, @Res() response: Response) {
+    console.log(request.url);
+    response.status(200).json({
+      message: 'Hello mina',
+    });
+  }
+
+  @Get('notfound')
+  @HttpCode(404)
+  notFoundPage() {
+    return '404 not found';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  nerrorPage() {
+    return '500 not erro';
+  }
+
+  ////
+
+  @Get('new')
+  @HttpCode(201)
+  somethingNew() {
+    return 'Something new';
+  }
+
+  @Get('ticket/:num')
+  getNumber(@Param('num', ParseIntPipe) num: number) {
+    console.log(typeof num);
+    return num + 14;
+  }
+
+  @Get('active/:status')
+  isUserActive(@Param('status', ParseBoolPipe) status: boolean) {
+    console.log(typeof status);
+    return status;
+  }
+
+
+
+  
+  @Get('greet')
+  greet(@Query(ValidateuserPipe) query: { name: string; age: number }) {
+    console.log(typeof query.age);
+    console.log(typeof query.name);
+    return `Hello ${query.name}, you are ${query.age+60} years old`;
+  }
+}
+
